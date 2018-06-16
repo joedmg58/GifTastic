cc8cbfa9d0950f349cc757b56cf3111c8531d868
Joed Machado <joedmg58@gmail.com>
2018-06-16 17:24:49 -0400

    JS completed, click event for gif is done

--- js/giftastic.js
+++ js/giftastic.js
@@ -1,5 +1,6 @@
 // Global variables
-const gifTastic_API_Key = 'wbAm7mfVgl724955m2TtsvfFZ2UKtM2v';
+const giphy_API_Key = 'wbAm7mfVgl724955m2TtsvfFZ2UKtM2v';
+const giphyURL = 'https://api.giphy.com/v1/gifs/search?';
 
 var animals = [
                 'dog',
@@ -7,7 +8,7 @@ var animals = [
                 'mouse',
                 'rabbit',
                 'wolf',
-                'leppard',
+                'leopard',
                 'frog',
                 'lion'
             ];
@@ -21,9 +22,61 @@ function createButtons() {
     }
 }   
 
+// Adding image to some container
+function addImage( container, still, animate  ){
+    $('<img>').attr({
+        'src':still,
+        'data-still': still,
+        'data-animate': animate,
+        'data-state': 'still',
+        'width': '200',
+        'height': '200'
+    }).addClass('animatedGIF').appendTo( container );
+}
+
+// Toggle animation function
+function gifClick( event ){
+    console.log( $(this).attr('data-still') );
+    var gif = $(this);
+    if ( gif.attr( 'data-state' ) === 'still' ) {
+        gif.attr( 'data-state', 'animate' );
+        gif.attr( 'src', gif.attr('data-animate') );
+    }else
+    if ( gif.attr( 'data-state' ) === 'animate' ) {
+        gif.attr( 'data-state', 'still' );
+        gif.attr( 'src', gif.attr('data-still') );
+    }
+}
+
 // Function for the on-click event of the button class giphyBtn
 function giphyBtnClick( event ) {
-    console.log( $(this).val() );
+    var animal = $(this).val();
+
+    console.log( animal );
+
+    $.ajax({
+        url: giphyURL  + 'api_key=' + giphy_API_Key + '&q=' + animal + '&limit=10',
+        method: 'GET'
+    }).done( function(response) {
+        
+        console.log( response );
+
+        var data = response.data;
+
+        $('#gif-container').empty;
+
+        for ( var i = 0; i < data.length; i++) {
+            var stillImage = data[i].images.downsized_still.url;
+            var animateImage = data[i].images.downsized.url;
+            var imageRating = data[i].rating;
+
+            console.log( stillImage );
+            console.log( animateImage );
+            console.log( imageRating );
+
+            addImage( $('#gif-container'), stillImage, animateImage );
+        }
+    });
 }
 
 //Function to add new animals
@@ -47,5 +100,8 @@ $( document ).ready( function() {
 
     // Adding click event listeners to all elements with a class of "giphyBtn"
     $(document).on( 'click', '.giphyBtn', giphyBtnClick );
+
+    //Adding click event listeners to all elements with tha class of "animatedGIF"
+    $(document).on( 'click', '.animatedGIF', gifClick );
 
 } );            
